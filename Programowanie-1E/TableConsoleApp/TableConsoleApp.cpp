8ebc5c86bc573d5603835f73a341c030419ab704
IronLittleFox <lukasz.felisek@gmail.com>
2023-09-28 13:45:22 +0200

    d

--- Programowanie-1E/TableConsoleApp/TableConsoleApp.cpp
+++ Programowanie-1E/TableConsoleApp/TableConsoleApp.cpp
@@ -177,6 +177,14 @@ void task5()
 
 }
 
+/*
+* Napisz funkcję, która dla kolekcji danych liczbowych policzy ile jest liczb większych od średniej arytmetycznej.
+* Napisz funkcje, która dla kolekcji danych liczbowych przeniesie te liczby do innej kolekcji w odwrotnej kolejności.
+* Napisz funkcje, która dla kolekcji danych liczbowych obliczy częstotliwość występowania danej liczby.
+* Napisz funkcję, która dla kolekcji danych liczbowych znajdzie najdłuższy rosnący podciąg.
+* Napisz funkcję, która dla kolekcji danych liczbowych przeniesie te liczby do osobnych kolekcji liczb parzystych i nieparzystych.
+*/
+
 int main()
 {
 	task4();
