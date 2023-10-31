75fc8ef4498fe40c420c73b065a0a6beef9b6e2d
IronLittleFox <lukasz.felisek@gmail.com>
2023-10-31 12:54:33 +0100

    koniec lekcji

---
+++ Programowanie-1E/TicTacToeConsoleApplication/TicTacToeConsoleApplication.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+int main()
+{
+    //inicjalizacja gry
+
+    //rysujemy planszę z aktualnym stanem
+
+    //wybranie pola na planszy przez aktualnego gracza
+
+    //sprawdzanie wygranej
+        //koniec gry
+
+    //sprawdzanie remisu
+        //koniec gry
+
+    //zmiana gracza na kolejnego
+
+
+}
+
+/*
+   | O | X 
+---+---+---
+   | X |
+---+---+---
+   |   | O
+
+   */
+
