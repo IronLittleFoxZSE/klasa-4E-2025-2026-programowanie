f7afdc38026ad97611a827ccfd82dfad52b4f673
IronLittleFox <lukasz.felisek@gmail.com>
2023-12-07 15:14:51 +0100

    sds

--- Programowanie-1E/LibraryConsoleApplication/LibraryConsoleApplication.cpp
+++ Programowanie-1E/LibraryConsoleApplication/LibraryConsoleApplication.cpp
@@ -1,6 +1,29 @@
 ﻿#include <iostream>
 
+struct book
+{
+    std::string title;
+    std::string author;
+    short publicationYear;
+    unsigned short numberOfPages;
+};
+
+
 int main()
 {
-    
+    const int BOOK_SIZE = 101;
+    book collectionOfBooks[BOOK_SIZE];
+
+    int size;
+    std::cin >> size;
+    book* colOfBooks = new book[size];
+    colOfBooks[0].title = "Lalka";
+
+    collectionOfBooks[0].author = "Adam Mickiewicz";
+    collectionOfBooks[0].title = "Dziady";
+    collectionOfBooks[0].publicationYear = 2003;
+    collectionOfBooks[0].numberOfPages = 79;
+
+
+
 }
