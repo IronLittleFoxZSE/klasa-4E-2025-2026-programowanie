2b0a8d4186ee673f3d4542c8d8f41bdfc62d50de
IronLittleFox <lukasz.felisek@gmail.com>
2023-12-14 15:09:15 +0100

    koniec lekcji

--- Programowanie-1E/LibraryConsoleApplication/LibraryConsoleApplication.cpp
+++ Programowanie-1E/LibraryConsoleApplication/LibraryConsoleApplication.cpp
@@ -2,27 +2,56 @@
 
 struct book
 {
-    std::string title;
-    std::string author;
-    short publicationYear;
-    unsigned short numberOfPages;
+	std::string title;
+	std::string author;
+	short publicationYear;
+	unsigned short numberOfPages;
 };
 
+void menu()
+{
+	system("cls");
+	std::cout << "BIBLIOTEKA\n";
+	std::cout << "1. Dodanie nowej książki.\n";
+
+	std::cout << "0. Koniec programu\n";
+}
+
+int getOption()
+{
+	std::cout << "Wybierz opcję:\n";
+	int option;
+	std::cin >> option;
+	return option;
+}
 
 int main()
 {
-    const int BOOK_SIZE = 101;
-    book collectionOfBooks[BOOK_SIZE];
+	const int BOOK_SIZE = 100;
+	book collectionOfBooks[BOOK_SIZE];
+	int currentNumberOfBooks = 0;
 
-    int size;
-    std::cin >> size;
-    book* colOfBooks = new book[size];
-    colOfBooks[0].title = "Lalka";
+	while (true)
+	{
+		menu();
+		int choosenOption = getOption();
 
-    collectionOfBooks[0].author = "Adam Mickiewicz";
-    collectionOfBooks[0].title = "Dziady";
-    collectionOfBooks[0].publicationYear = 2003;
-    collectionOfBooks[0].numberOfPages = 79;
+		if (choosenOption == 0)
+			break;
+
+		switch (choosenOption)
+		{
+		case 1:
+			addNewBook(collectionOfBooks, currentNumberOfBooks);
+			break;
+		}
+	}
+
+
+	/*collectionOfBooks[0].author = "Adam Mickiewicz";
+	collectionOfBooks[0].title = "Dziady";
+	collectionOfBooks[0].publicationYear = 2003;
+	collectionOfBooks[0].numberOfPages = 79;*/
 
 
 
