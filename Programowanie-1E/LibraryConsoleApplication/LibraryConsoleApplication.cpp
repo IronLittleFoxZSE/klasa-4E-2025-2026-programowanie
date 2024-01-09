e15e1fee215f49bb1116576d1271c0e1cf4481a0
Lukasz <lukasz.felisek@ezse.pl>
2024-01-09 15:04:36 +0100

    koniec zajęć

--- Programowanie-1E/LibraryConsoleApplication/LibraryConsoleApplication.cpp
+++ Programowanie-1E/LibraryConsoleApplication/LibraryConsoleApplication.cpp
@@ -13,6 +13,7 @@ void menu()
 	system("cls");
 	std::cout << "BIBLIOTEKA\n";
 	std::cout << "1. Dodanie nowej książki.\n";
+	std::cout << "2. Wyświetlenie wszystkich książek\n";
 
 	std::cout << "0. Koniec programu\n";
 }
@@ -25,11 +26,40 @@ int getOption()
 	return option;
 }
 
+void addNewBook(book books[], int &currentNumberOfBooks)
+{
+	currentNumberOfBooks++;
+	std::cout << "Podaj tytuł:\n";
+	std::cin >> books[currentNumberOfBooks].title;
+	std::cout << "Podaj autora:\n";
+	std::cin >> books[currentNumberOfBooks].author;
+	std::cout << "Podaj rok publickacji:\n";
+	std::cin >> books[currentNumberOfBooks].publicationYear;
+	std::cout << "Podaj ilość stron:\n";
+	std::cin >> books[currentNumberOfBooks].numberOfPages;
+}
+
+void showAllBooks(book books[], int currentNumberOfBooks)
+{
+	system("cls");
+	std::cout << "Wyświetlenie wszystkich pozycji:\n";
+	for (int i = 0; i <= currentNumberOfBooks; i++)
+	{
+		std::cout << "------------------------------------------\n";
+		std::cout << "Tytuł: " << books[i].title << "\n";
+		std::cout << "Autor: " << books[i].author << "\n";
+		std::cout << "Rok wydania: " << books[i].publicationYear << "\n";
+		std::cout << "Ilość stron: " << books[i].numberOfPages << "\n";
+	}
+	system("pause");
+}
+
 int main()
 {
+	setlocale(LC_CTYPE, "polish");
 	const int BOOK_SIZE = 100;
 	book collectionOfBooks[BOOK_SIZE];
-	int currentNumberOfBooks = 0;
+	int currentNumberOfBooks = -1;
 
 	while (true)
 	{
@@ -43,6 +73,9 @@ int main()
 		{
 		case 1:
 			addNewBook(collectionOfBooks, currentNumberOfBooks);
+			break;
+		case 2:
+			showAllBooks(collectionOfBooks, currentNumberOfBooks);
 			break;
 		}
 	}
