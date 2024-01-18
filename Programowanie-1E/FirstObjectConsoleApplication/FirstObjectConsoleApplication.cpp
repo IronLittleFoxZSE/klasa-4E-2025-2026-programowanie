0abbe812bdacf8a7a8245d223bf89fc0cd0b0786
g <g@g.pl>
2024-01-18 15:10:52 +0100

    koniec zajęć

---
+++ Programowanie-1E/FirstObjectConsoleApplication/FirstObjectConsoleApplication.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+struct person
+{
+	std::string name;
+	//std::string secondName;
+	std::string surname;
+	//short height;
+	short age;
+	//char sex; //'M' - male; 'F' - female
+};
+
+int main()
+{
+	person firstPerson;
+
+	firstPerson.name = "Jan";
+	firstPerson.surname = "Kowalski";
+	firstPerson.age = 5;
+
+	std::cout << "Info o osobie:\n";
+	std::cout << "Imię: " << firstPerson.name << "\n";
+	std::cout << "Nazwisko: " << firstPerson.surname << "\n";
+	std::cout << "Wiek: " << firstPerson.age << "\n";
+
+	person secondPerson;
+	secondPerson.name = "Paweł";
+	secondPerson.surname = "Nowak";
+	secondPerson.age = 49;
+
+	std::cout << "Info o osobie:\n";
+	std::cout << "Imię: " << secondPerson.name << "\n";
+	std::cout << "Nazwisko: " << secondPerson.surname << "\n";
+	std::cout << "Wiek: " << secondPerson.age << "\n";
+}
