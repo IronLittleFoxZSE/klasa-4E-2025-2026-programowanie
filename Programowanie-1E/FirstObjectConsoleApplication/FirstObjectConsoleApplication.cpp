3c534441acd11924e0022ee4b2ff5ac7ecca619f
g <g@g.pl>
2024-01-23 15:15:01 +0100

    koniec lekcji

--- Programowanie-1E/FirstObjectConsoleApplication/FirstObjectConsoleApplication.cpp
+++ Programowanie-1E/FirstObjectConsoleApplication/FirstObjectConsoleApplication.cpp
@@ -1,35 +1,47 @@
 ﻿#include <iostream>
 
-struct person
+class person
 {
+public:
 	std::string name;
 	//std::string secondName;
 	std::string surname;
 	//short height;
 	short age;
 	//char sex; //'M' - male; 'F' - female
+
+	void showInfo()
+	{
+		std::cout << "Info o osobie:\n";
+		std::cout << "Imię: " << name << "\n";
+		std::cout << "Nazwisko: " << surname << "\n";
+		std::cout << "Wiek: " << age << "\n";
+	}
+
+	bool isLegalAge()
+	{
+		return age >= 18;
+	}
 };
 
 int main()
 {
 	person firstPerson;
-
 	firstPerson.name = "Jan";
 	firstPerson.surname = "Kowalski";
 	firstPerson.age = 5;
 
-	std::cout << "Info o osobie:\n";
-	std::cout << "Imię: " << firstPerson.name << "\n";
-	std::cout << "Nazwisko: " << firstPerson.surname << "\n";
-	std::cout << "Wiek: " << firstPerson.age << "\n";
+	firstPerson.showInfo();
+
+	if (firstPerson.isLegalAge())
+		std::cout << "Jesteś pełnoletni\n";
 
 	person secondPerson;
 	secondPerson.name = "Paweł";
 	secondPerson.surname = "Nowak";
 	secondPerson.age = 49;
 
-	std::cout << "Info o osobie:\n";
-	std::cout << "Imię: " << secondPerson.name << "\n";
-	std::cout << "Nazwisko: " << secondPerson.surname << "\n";
-	std::cout << "Wiek: " << secondPerson.age << "\n";
+	secondPerson.showInfo();
+	if (secondPerson.isLegalAge())
+		std::cout << "Jesteś pełnoletni\n";
 }
