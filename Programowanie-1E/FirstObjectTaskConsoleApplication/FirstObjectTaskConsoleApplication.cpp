0c2103d3ace84022d6f1320650831fbb974e78d8
g <g@g.pl>
2024-02-29 13:52:43 +0100

    rozpoczęcie zadania

---
+++ Programowanie-1E/FirstObjectTaskConsoleApplication/FirstObjectTaskConsoleApplication.cpp
@@ -0,0 +1,48 @@
+﻿// FirstObjectTaskConsoleApplication
+//
+#include <iostream>
+
+/*
+Zdefiniuj klase opisujący punkt na przestrzeni kartezjańskiej. Możliwości klasy:
+* konstruktory
+* metoda która zwórci odległość tego punktu od poczatku układu współrzednych
+* metoda która zwróci odległość od innego punktu
+* metoda która zwróci informację w której ćwiartce układu współrzednych się znajduje punkt
+* metodę która pokaże informacje o punkcie.
+
+Napisz program który zaprezentuje możliwości obiektu na podstawie tej klasy.
+
+*/
+
+class Point2D
+{
+private:
+    double x;
+    double y;
+
+public:
+    Point2D()
+    {
+        x = 10;
+        y = 6;
+    }
+
+    Point2D(double xx)
+    {
+        x = xx;
+        y = 6;
+    }
+
+    Point2D(double xx, double yy)
+    {
+        x = xx;
+        y = yy;
+    }
+};
+
+int main()
+{
+    Point2D firstPoint2D;
+
+    Point2D secondPoint2D(2, 8.5);
+}
