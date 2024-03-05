6a93f5325782ba1768eadbdfe47f424460f248aa
g <g@g.pl>
2024-03-05 15:08:34 +0100

    koniec lekcji

--- Programowanie-1E/FirstObjectTaskConsoleApplication/FirstObjectTaskConsoleApplication.cpp
+++ Programowanie-1E/FirstObjectTaskConsoleApplication/FirstObjectTaskConsoleApplication.cpp
@@ -17,32 +17,49 @@ Napisz program który zaprezentuje możliwości obiektu na podstawie tej klasy.
 class Point2D
 {
 private:
-    double x;
-    double y;
+	double x;
+	double y;
 
 public:
-    Point2D()
-    {
-        x = 10;
-        y = 6;
-    }
+	Point2D()
+	{
+		x = 10;
+		y = 6;
+	}
 
-    Point2D(double xx)
-    {
-        x = xx;
-        y = 6;
-    }
+	Point2D(double xx)
+	{
+		x = xx;
+		y = 6;
+	}
 
-    Point2D(double xx, double yy)
-    {
-        x = xx;
-        y = yy;
-    }
+	Point2D(double xx, double yy)
+	{
+		x = xx;
+		y = yy;
+	}
+
+	double DistanceFromCenter()
+	{
+		double distance = sqrt(x * x + y * y);
+		return distance;
+	}
+
+	double DiastanceBetweenPoints(Point2D secondPoint)
+	{
+		double distance = sqrt((x - secondPoint.x) * (x - secondPoint.x) + (y - secondPoint.y) * (y - secondPoint.y));
+		return distance;
+	}
 };
 
 int main()
 {
-    Point2D firstPoint2D;
+	Point2D firstPoint2D;
+	std::cout << "Odległość od środka to " << firstPoint2D.DistanceFromCenter() << "\n";
+	Point2D secondPoint2D(2, 8.5);
 
-    Point2D secondPoint2D(2, 8.5);
+	std::cout << "Odległość pomiędzy dwoma punktami " << secondPoint2D.DiastanceBetweenPoints(firstPoint2D) << "\n";
 }
+
+//"Zawsze programuj jak gdyby osoba zajmująca się twoim kodem w przyszłości była agresywnym psychopatą,
+//który wie gdzie mieszkasz."
