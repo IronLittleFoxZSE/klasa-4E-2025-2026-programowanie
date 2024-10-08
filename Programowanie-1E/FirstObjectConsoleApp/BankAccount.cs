067f43b52afc5b09e6ce103f67ab99bbd74cc367
f <f@f.pl>
2024-10-08 08:24:23 +0200

    pierwszy program obiektowy

---
+++ Programowanie-1E/FirstObjectConsoleApp/BankAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstObjectConsoleApp
+{
+    internal class BankAccount
+    {
+        public double balance; //saldo
+        public string pin;
+        public string owner; //właściciel
+        public string currency; //waluta
+    }
+}
---
+++ Programowanie-1E/FirstObjectConsoleApp/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+using FirstObjectConsoleApp;
+
+BankAccount firstAccount = new BankAccount();
+firstAccount.balance = 10;
+firstAccount.currency = "zł";
+firstAccount.owner = "Jan Kowalski";
+
+BankAccount secondAccount = new BankAccount();
+secondAccount.balance = 20;
+secondAccount.currency = "zł";
+secondAccount.owner = "Ewa Nowak";
+
+showInfo(firstAccount);
+showInfo(secondAccount);
+
+depositToAccount(ref firstAccount, -5);
+showInfo(firstAccount);
+
+widthdrawalFromAccount(ref secondAccount, 25);
+showInfo(secondAccount);
+
+transferBetweenAccounts(ref firstAccount, ref secondAccount, 300);
+showInfo(firstAccount);
+showInfo(secondAccount);
+
+void showInfo(BankAccount account)
+{
+    Console.WriteLine("Informacja o koncie bankowym");
+    Console.WriteLine($"Właściciel {account.owner}");
+    Console.WriteLine($"Saldo {account.balance} {account.currency}");
+}
+
+void depositToAccount(ref BankAccount account, double amount)
+{
+    if (amount >= 0)
+        account.balance = account.balance + amount;
+}
+
+bool widthdrawalFromAccount(ref BankAccount account, double amount)
+{
+    if (amount >= 0
+        && amount <= account.balance)
+    {
+        account.balance = account.balance - amount;
+        return true;
+    }
+    return false;
+}
+
+void transferBetweenAccounts(ref BankAccount sourceAccount, ref BankAccount targetAccount, double amount)
+{
+    if (widthdrawalFromAccount(ref sourceAccount, amount) == true)
+        depositToAccount(ref targetAccount, amount);
+}
