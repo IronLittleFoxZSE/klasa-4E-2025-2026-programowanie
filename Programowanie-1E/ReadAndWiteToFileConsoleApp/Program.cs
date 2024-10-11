5a4f4a48fef6f0c7dc79e274fafc1eb96a680c23
f <f@f.pl>
2024-10-11 12:28:30 +0200

    Koniec lekcji

---
+++ Programowanie-1E/ReadAndWiteToFileConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+//ReadAndWriteToFileConsoleApp
+
+//Zapis do pliku
+
+string name;
+int age;
+Console.WriteLine("Podaj imie");
+name = Console.ReadLine();
+Console.WriteLine("Podaj wiek");
+age = int.Parse(Console.ReadLine());
+
+StreamWriter streamWriter = new StreamWriter("c:\\plik3E.txt", true);
+
+streamWriter.WriteLine(name);
+streamWriter.Flush();
+streamWriter.WriteLine(age);
+
+streamWriter.Close();
+
+StreamReader streamReader = new StreamReader("c:\\plik3E.txt");
+
+name = streamReader.ReadLine();
+age = int.Parse(streamReader.ReadLine());
+
+Console.WriteLine($"Odczytano z pliku {name} {age}");
+
+streamReader.Close();
