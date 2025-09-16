59f6349ba92e8d8f843729762bd7df5f595adea9
f <f@f.pl>
2025-09-16 12:54:54 +0200

    Dodanie projektów

---
+++ Programowanie-1E/EventVsBindingMauiApp/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace EventVsBindingMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public string Message { get; set; }
+
+        private string returnMessage;
+        public string ReturnMessage
+        {
+            get { return returnMessage; }
+            set 
+            { 
+                returnMessage = value;
+                //OnPropertyChanged("ReturnMessage");
+                //OnPropertyChanged(nameof(ReturnMessage));
+                OnPropertyChanged();
+            }
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            rotationLabel.Rotation = rotationSlider.Value;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            ReturnMessage = "Ilość znaków " + Message.Length;
+            //OnPropertyChanged("ReturnMessage");
+            //OnPropertyChanged(nameof(ReturnMessage));
+        }
+    }
+}
---
+++ Programowanie-1E/SimpleCalculatorMauiApp/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace SimpleCalculatorMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (!int.TryParse(firstNumberEntry.Text, out int firstNumber)
+                || !int.TryParse(secondNumberEntry.Text, out int secondNumber))
+                return;
+            
+            int result = firstNumber + secondNumber;
+
+            resultLabel.Text = "Wynik to: " + result.ToString();
+        }
+    }
+
+}
