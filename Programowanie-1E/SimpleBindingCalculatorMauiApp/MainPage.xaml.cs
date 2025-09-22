d98065539be25551993eb183ba69da85ace39a83
f <f@f.pl>
2025-09-22 10:40:00 +0200

    Zrobienie zadania

---
+++ Programowanie-1E/SimpleBindingCalculatorMauiApp/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace SimpleBindingCalculatorMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public string StrFirstNumber { get; set; }
+        public string StrSecondNumber { get; set; }
+
+        private string resultMessage;
+        public string ResultMessage
+        {
+            get { return resultMessage; }
+            set { resultMessage = value; OnPropertyChanged(); }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (!int.TryParse(StrFirstNumber, out int firstNumber)
+                || !int.TryParse(StrSecondNumber, out int secondNumber))
+                return;
+
+            int result = firstNumber + secondNumber;
+
+            ResultMessage = "Wynik to: " + result.ToString();
+        }
+    }
+}
